5feb157b8d6c074b28577231abde8efb1b8ab227
KitSymes <kitsymes@btinternet.com>
2021-11-05 20:21:35 +0000

    Commit #016
    - Make navbar shorter when in max view
    - Add support for image-to-video on hover
    - Reworded skills section
    - Fixed dead SDLProject->SDL-Emblem links

---
+++ assets/js/image-to-video.js
@@ -0,0 +1,32 @@
+var enabled = false;
+
+function showVideo(obj)
+{
+	if (!enabled)
+		return;
+	$obj = $(obj);
+    $obj.hide();
+	$vid = $obj.siblings('.video');
+	$vid.show();
+	$vid.find("iframe").attr("src", $obj.attr("data-src"));
+}
+
+function hideVideo(obj)
+{
+	if (!enabled)
+		return;
+	$obj = $(obj);
+    $obj.hide();
+	$obj.siblings('.image').show();
+}
+
+function onYouTubeIframeAPIReady() {
+  $player = new YT.Player('youtube', {
+    events: {
+      'onReady': function (event) {
+        event.target.setVolume(0);
+        event.target.playVideo();
+      }
+    }
+  });
+}
